8f26af830f8f130c80ff220544927b2eb39dea50
chubbyyb <keithsalhani7@gmail.com>
2023-07-03 18:47:36 +0100

    modified:   bot.py

--- bot.py
+++ bot.py
@@ -15,26 +15,31 @@ channel_id = 1053499749443059814
 
 
 
-# Get weather, using weatherapi.com
-key = "REPLACE WITH weatherapi.com KEY"
-response = requests.get(f"http://api.weatherapi.com/v1/forecast.json?key={key}&q=Dublin&days=1&aqi=no&alerts=no")
-data = response.json()
-forecast = data["forecast"]["forecastday"][0]
-max_temp_celsius = forecast["day"]["maxtemp_c"]
-min_temp_celsius = forecast["day"]["mintemp_c"]
-conditions = forecast["day"]["condition"]["text"]
-weather_icon_URL = forecast["day"]["condition"]["icon"]
+def getWeather():
+    # Get weather, using weatherapi.com
+    key = "REPLACE WITH weatherapi.com KEY"
+    response = requests.get(f"http://api.weatherapi.com/v1/forecast.json?key={key}&q=Dublin&days=1&aqi=no&alerts=no")
+    data = response.json()
+    forecast = data["forecast"]["forecastday"][0]
+    max_temp_celsius = forecast["day"]["maxtemp_c"]
+    min_temp_celsius = forecast["day"]["mintemp_c"]
+    conditions = forecast["day"]["condition"]["text"]
+    weather_icon_URL = forecast["day"]["condition"]["icon"]
 
+    return max_temp_celsius, min_temp_celsius, conditions, weather_icon_URL
 
-# Get news, using newsapi.org
-key = "REPLACE WITH newsapi.org KEY"
-# response = requests.get(f"https://newsapi.org/v2/top-headlines?country=ie&apiKey={key}") NORMAL NEWS
-response = requests.get(f"https://newsapi.org/v2/top-headlines?country=ie&category=technology&apiKey={key}") # TECH NEWS
-data = response.json()
-articles = data["articles"]
-headline_one = articles[0]["title"]
-headline_two = articles[1]["title"]
-headline_three = articles[2]["title"]
+def getNews():
+    # Get news, using newsapi.org
+    key = "REPLACE WITH newsapi.org KEY"
+    # response = requests.get(f"https://newsapi.org/v2/top-headlines?country=ie&apiKey={key}") NORMAL NEWS
+    response = requests.get(f"https://newsapi.org/v2/top-headlines?country=ie&category=technology&apiKey={key}") # TECH NEWS
+    data = response.json()
+    articles = data["articles"]
+    headline_one = articles[0]["title"]
+    headline_two = articles[1]["title"]
+    headline_three = articles[2]["title"]
+
+    return headline_one, headline_two, headline_three
 
 
 def getVideo():
@@ -143,11 +148,15 @@ async def on_message(message):
 async def send_message():
     print(time.localtime().tm_hour)
     if time.localtime().tm_hour == 6:
+        
+        weatherData = getWeather()
+        newsData = getNews()
+
         channel = client.get_channel(channel_id)
         print(channel)
         embed=discord.Embed(title="Good Morning!", 
-                                description=f"**Todays weather in Dublin:**\n{conditions}\nmin: {min_temp_celsius}c\nmax: {max_temp_celsius}c\n\n**Todays News:**\n{headline_one}\n\n{headline_two}\n\n{headline_three}\n\n**Fireship Videos:**\n{getVideo()}\n\n**Have a great day!**", 
+                                description=f"**Todays weather in Dublin:**\n{weatherData[2]}\nmin: {weatherData[1]}c\nmax: {weatherData[0]}c\n\n**Todays News:**\n{newsData[0]}\n\n{newsData[1]}\n\n{newsData[2]}\n\n**Fireship Videos:**\n{getVideo()}\n\n**Have a great day!**", 
                                 color=0x00ff00)
-        embed.set_thumbnail(url=F"https:{weather_icon_URL}")
+        embed.set_thumbnail(url=F"https:{weatherData[3]}")
         embed.set_image(url=gmGifs[0])
         await channel.send(embed=embed)
