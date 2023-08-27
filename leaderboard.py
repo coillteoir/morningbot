1c7e27da778bd32258822de45a9e4cb3ab5cac00
David <davite3@gmail.com>
2023-08-27 13:43:54 +0100

    member now subclass of leaderboard

--- leaderboard.py
+++ leaderboard.py
@@ -5,22 +5,24 @@
 import json
 import os
 
-class Member:
-    def __init__(self, name, mornings):
-        self.name = name
-        self.mornings = mornings
 
 class Leaderboard:
+
+    class Member:
+        def __init__(self, name, mornings):
+            self.name = name
+            self.mornings = mornings
+
     def __init__(self, channel):
         path = "config/{}leaderboard.json".format(channel)
+        print(path)
         self.members = []
         if os.path.isfile(path):
             fp = open(path, "r")
             leader_dict = json.loads(fp)
             self.channel = leader_dict["channel"]
             for x in leader_dict:
-                self.members.append(Member(x["name"], x["mornings"]))
-            
+                self.members.append(self.Member(x["name"], x["mornings"]))
         else:
             self.channel = channel
 
