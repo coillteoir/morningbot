0cef0be89f4896fe841836c34d259750d3b42564
David <davite3@gmail.com>
2023-08-28 17:44:36 +0100

    more progress and test scenario

--- leaderboard/__init__.py
+++ leaderboard/__init__.py
@@ -11,16 +11,36 @@ class Leaderboard:
         def __init__(self, name, mornings):
             self.name = name
             self.mornings = mornings
+        def __str__(self):
+            return f"NAME: {self.name} SCORE: {self.mornings}"
 
     def __init__(self, channel):
+        print("LEADERBOARD INIT")
         path = "config/{}leaderboard.json".format(channel)
         print(path)
+
         self.members = []
         if os.path.isfile(path):
-            fp = open(path, "r")
-            leader_dict = json.loads(fp)
+            with open(path, "r") as leader_file:
+                leader_dict = json.loads(leader_file)
             self.channel = leader_dict["channel"]
+
             for x in leader_dict:
                 self.members.append(self.Member(x["name"], x["mornings"]))
         else:
             self.channel = channel
+    def add_point(self, name):
+        for x in self.members:
+            if x.name == name:
+                x.mornings += 1
+                print("exists", x)
+                break
+        else:
+            self.members.append(self.Member(name, 1))
+            for x in self.members:
+                print("created", x)
+
+    def dump_data(self):
+        path = f"config/{self.channel}leaderboard.json"
+
+
